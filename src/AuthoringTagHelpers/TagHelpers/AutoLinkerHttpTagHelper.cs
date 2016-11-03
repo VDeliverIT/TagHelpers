49f5e3a91dd99ba64ea59ab5f48ab33294fb0ae1
sunyatravel <yali.girijaa@gmail.com>
2016-11-03 13:53:11 +0530

    Tag Helper
    First commit to local

---
+++ src/AuthoringTagHelpers/TagHelpers/AutoLinkerHttpTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AuthoringTagHelpers.TagHelpers
+{
+    [HtmlTargetElement("p")]
+    public class AutoLinkerHttpTagHelper : TagHelper
+    {
+        public override int Order
+        {
+            get { return int.MinValue; }
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var childContent = await output.GetChildContentAsync();
+            // Find Urls in the content and replace them with their anchor tag equivalent.
+            output.Content.SetHtmlContent(Regex.Replace(
+                 childContent.GetContent(),
+                 @"\b(?:https?://)(\S+)\b",
+                  "<a target=\"_blank\" href=\"$0\">$0</a>"));  // http link version}
+        }
+    }
+}
---
+++ src/AuthoringTagHelpers/TagHelpers/BoldTagHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AuthoringTagHelpers.TagHelpers
+{
+    // [HtmlTargetElement("bold")]
+    // [HtmlTargetElement(Attributes = "bold")]        /* Logical OR bold tag or bold attribute */
+
+    // [HtmlTargetElement("bold", Attributes = "bold")] /* Logical AND bold tag with bold attribute */
+    [HtmlTargetElement("MyBold")] /* Applies only to MyBold tag */
+    public class BoldTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll("bold");
+            output.PreContent.SetHtmlContent("<strong>");
+            output.PostContent.SetHtmlContent("</strong>");
+        }
+    }
+}
+
+/*
+ * public IActionResult About()
+{
+    ViewData["Message"] = "Your application description page.";
+    return View("AboutBoldOnly");
+    // return View();
+}
+*/
